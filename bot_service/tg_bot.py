5281d38c60daecc7230d6015c7a7f49945c3e144
TrySoony <125113485+TrySoony@users.noreply.github.com>
2025-06-29 11:55:45 +0300

    Add files via upload

--- bot_service/tg_bot.py
+++ bot_service/tg_bot.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 import logging
 import os
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_ADMIN_ID
@@ -10,7 +11,10 @@ from trader import AggressiveFuturesTrader
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("tg_bot")
 
-bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=TELEGRAM_BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 trader = AggressiveFuturesTrader()
